8d1741391d20b2884afee086e2f1b9b7b0233ac1
Rafal Lindemann <rl@stamina.pl>
2013-01-23 00:06:13 +0100

    tests

--- tests/OOIO/Test/StreamTest.php
+++ tests/OOIO/Test/StreamTest.php
@@ -38,4 +38,40 @@ class StreamTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals("Hello World", $this->stream->read());
     }
+    
+    function testMemory()
+    {
+        $string = 'fooBAR';
+        $stream = \OOIO\IO::memory(false, $string);
+        
+        $this->assertEquals($string, $stream->read());
+        $this->assertEquals(strlen($string), $stream->stat()->size);
+        $this->assertEquals(strlen($string), $stream->tell());
+        
+        $stream->seek(3);
+        $this->assertEquals(substr($string, 3), $stream->read());
+        
+        $stream->seek(3);
+        $stream2 = \OOIO\IO::memory(false, $stream);
+        $this->assertEquals(substr($string, 3), $stream2->read());
+    }
+    
+    function testCopy() 
+    {
+        $this->stream->write("Hello World");
+        $this->stream->rewind();
+        
+        $stream2 = \OOIO\IO::memory();
+        
+        $this->stream->copy($stream2, 5);
+        
+        $stream2->rewind();
+        $this->assertEquals('Hello', $stream2->read());
+        
+        $stream2->copyFrom($this->stream);
+        
+        $stream2->rewind();
+        $this->assertEquals('Hello World', $stream2->read());
+    }
+    
 }
